34377bd8c0b8bdac2915ca0dcdf5de20eb639eb4
Vibhor Nanavati <vibhor@dhcp-172-19-25-20.mtv.corp.google.com>
2010-11-02 14:53:57 -0700

    Allow submitter to specify team from current semester.

--- app/models/deliverable_submission.rb
+++ app/models/deliverable_submission.rb
@@ -1,11 +1,14 @@
 class DeliverableSubmission < ActiveRecord::Base
   belongs_to :person
   belongs_to :course
+  belongs_to :team
 
-  # TODO(vibhor): The person will eventually be the signed in user.
   validates_presence_of :person
   validates_presence_of :submission_date
   validates_associated :person
+  # This validation is not entirely true as there can be individual deliverable submissions.
+  # TODO(vibhor): Fix this once we bring in the notion of individual deliverable.
+  validates_presence_of :team
 
   # TODO(vibhor): Update to use amazon s3 as storage.
   has_attached_file :deliverable,
